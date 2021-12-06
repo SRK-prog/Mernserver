326b454a1591a07f4ecdbca3feca2726ef328245
SRK-prog <sivaram262@gmail.com>
2021-12-06 10:24:52 +0530

    changes

--- index.js
+++ index.js
@@ -7,33 +7,17 @@ const postRoute = require("./routes/posts");
 const conversationRoute = require("./routes/conversations");
 const messageRoute = require("./routes/messages");
 const mailRoute = require("./routes/mails");
-const multer = require("multer");
 const cors = require("cors");
 const path = require("path");
 
 const app = express();
 dotenv.config();
 app.use(express.json());
-app.use("/images", cors(), express.static(path.join(__dirname, "/images")));
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(console.log("connected to mongo"))
   .catch((err) => console.log(err));
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
-});
-
-const upload = multer({ storage: storage });
-app.post("/api/upload", cors(), upload.single("file"), (req, res) => {
-  res.status(200).json("File has been uploaded");
-});
 
 app.use("/api/auth", cors(), authRoute);
 app.use("/api/users", cors(), userRoute);
@@ -42,6 +26,6 @@ app.use("/api/conversations", cors(), conversationRoute);
 app.use("/api/messages", cors(), messageRoute);
 app.use("/api/mail", cors(), mailRoute);
 
-app.listen(process.env.HEROKU_URL || 5000, () => {
+app.listen(5000, () => {
   console.log("server running");
 });
