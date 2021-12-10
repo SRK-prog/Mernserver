89f452359b3bdcef95cbf9b411be7d88e3afaf5a
SRK-prog <sivaram262@gmail.com>
2021-12-10 12:51:15 +0530

    changes

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,6 @@ router.post("/signup", async (req, res) => {
   try {
     const salt = await bcrypt.genSalt(10);
     const hashedPass = await bcrypt.hash(req.body.password, salt);
-
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
@@ -25,18 +24,19 @@ router.post("/signup", async (req, res) => {
 
 router.post("/login", async (req, res) => {
   try {
-    const user = await User.findOne({ username: req.body.username });
-    !user && res.status(400).json("Wrong credentials!");
-
+    const user = await User.findOne({ email: req.body.email });
+    !user &&
+      res
+        .status(400)
+        .json("The email address you entered isn't connected to an account.");
     const validated = await bcrypt.compare(req.body.password, user.password);
-    !validated && res.status(400).json("Wrong credentials!");
-
+    !validated &&
+      res.status(400).json("The password that you've entered is incorrect.");
     const { password, ...others } = user._doc;
     res.status(200).json(others);
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
 
 module.exports = router;
