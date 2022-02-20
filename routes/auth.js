3ad17d107e79fa144662df3fba4cb95dacea5e28
SRK-prog <sivaram262@gmail.com>
2022-02-20 19:01:52 +0530

    fixing bugs

--- routes/auth.js
+++ routes/auth.js
@@ -25,20 +25,18 @@ router.post("/signup", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
-    if(user === null){
+    if (user === null) {
       res
         .status(400)
-        .json("The email address you entered isn't connected to an account.")
-    }
-    else{
-    	const validated = await bcrypt.compare(req.body.password, user.password);
-       if(!validated){
-          res.status(400).json("The password that you've entered is incorrect.")
-       }
-    	else{
-	  const { password, ...others } = user._doc;
-	  res.status(200).json(others)
-    	}
+        .json("The email address you entered isn't connected to an account.");
+    } else {
+      const validated = await bcrypt.compare(req.body.password, user.password);
+      if (!validated) {
+        res.status(400).json("The password that you've entered is incorrect.");
+      } else {
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+      }
     }
   } catch (err) {
     res.status(500).json(err);
